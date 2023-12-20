792d1e40ab416511f06231616f461ced5ddabb1c
leminhduy1912 <minhduyle081003@gmail.com>
2023-12-20 18:34:03 +0700

    first commit

---
+++ src/main/java/com/o2AuthBasic/O2AuthBasicApplication.java
@@ -0,0 +1,13 @@
+package com.o2AuthBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2AuthBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2AuthBasicApplication.class, args);
+	}
+
+}
